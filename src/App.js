20032498140a4383fe9be821c6f0cb08a1be82ea
ShreyashNandgaonkar <shreyash.nandgaonkar7@gmail.com>
2020-04-19 17:19:07 +0530

    final code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+import Metronome from './Components/Metronome';
+
+class App extends Component {
+  render(){
+  return (
+    
+    <div className="App">
+    <Metronome />
+    </div>
+  );
+}
+}
+
+export default App;
